b07476c1f59ba1f28c93365aa20b571492951a02
nrngxv <nirneet.nishant@gmail.com>
2024-07-21 17:11:46 +0530

    Program functional on CLI

--- data_entry.py
+++ data_entry.py
@@ -1,13 +1,46 @@
 from datetime import datetime
 
-def get_date(prompt, allow_default=False):
-    pass
+
+date_format = "%d-%m-%Y"
+CATEGORIES = {"I": "Income", "E":"Expense"}
+
+
+def get_date(prompt, allow_default=False): #set allow_default to True to auto set default value
+    date_str = input(prompt) #pass value from user input to a variable
+
+    if allow_default and not date_str: #this allows for reverting back to default is user input is not present
+        #strftime converts date into string format (date-month-year)
+        return datetime.today().strftime("%d-%m-%Y") 
+    
+    try:
+        #converting the input into the datetime format d-m-y
+        valid_date = datetime(date_str, "%d-%m-%y") #NOTE: Removed .strftime
+        #returing the datetime as a string 
+        return valid_date.strftime("%d-%m-%y") 
+    except ValueError:
+        print("Invalid date format. Please enter the date in dd-mm-yyyy format")
+        return get_date(prompt, allow_default)
+
 
 def get_amount():
-    pass
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <=0: #validates the input for being right, if not then raise ValueError
+            raise ValueError("Amount must be a non-negetive non-zero value")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()
+
 
 def get_category():
-    pass
+    category = input("Enter the category ('I' for income or 'E' for expense): ").upper()
+    if category in CATEGORIES:
+        return CATEGORIES[category] #this is calling the value from the dictionary CATEGORIES
+    else:
+        print("Invalid category. Please enter 'I' for income or 'E' for expense")
+        return get_category()
+
 
 def get_description():
-    pass
+    return input("Enter a description (Optional): ")
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #This files handles the main flow of the program
 
 #importing different modules
-
 import pandas as pd # this allows to load .csv file and work with it
 import csv 
 from datetime import datetime
+from data_entry import get_date, get_amount, get_category, get_description
 
 #store data in a .csv file
-
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
 
     @classmethod
-    def initialise_csv(cls): #this is to load the CSV file, and structure it
+    #this is to load the CSV file, and structure it
+    def initialise_csv(cls):
+
         try:
             pd.read_csv(cls.CSV_FILE) #Loading in CSV_FILE variable 
         except FileNotFoundError: #if the file does not exists
@@ -34,13 +35,19 @@ class CSV:
             writer.writerow(new_entry)
         print("Enter added successfully")
 
-CSV.initialise_csv()
-CSV.add_entry("20-07-2024", 120, "Expense", "Medicines")
+def add():
+    CSV.initialise_csv()
+    date = get_date("Enter the date of the transaction (dd-mm-YYYY format) or enter today's date: ", allow_default=True)
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
 
 
+# ***main flow***
 
+add()
 
 
 #TODO: view different transactions
 #TODO: create a nice interface to view transactions
-
